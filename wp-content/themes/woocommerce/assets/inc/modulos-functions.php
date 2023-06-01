a2108e1bda81ef739783ff0c0be25501abac597e
ixmaelx <122129728+ixmaelx@users.noreply.github.com>
2023-05-31 20:37:15 -0400

    scss en blanco

--- wp-content/themes/woocommerce/assets/inc/modulos-functions.php
+++
@@ -1,3 +0,0 @@
-<?php
-include get_template_directory() . '/assets/modulos/modulo-spotify/core-modulo-discos.php';
-include get_template_directory() . '/assets/modulos/modulo-spotify/core-modulo-canciones.php';
