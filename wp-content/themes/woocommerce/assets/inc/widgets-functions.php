838cd02d1dc0b9d9756a431e54df5378f60482f3
ixmaelx <pabloxguionbajo17@gmail.com>
2023-06-06 01:59:12 -0400

    lunes 5 noche
    se agrego widgets para carrusel
    se agregaron los widgets a l home
    se agrego la seccion de metodos de pago bajo el carrusel

--- wp-content/themes/woocommerce/assets/inc/widgets-functions.php
+++ wp-content/themes/woocommerce/assets/inc/widgets-functions.php
@@ -1,24 +1,56 @@
-<?php  
-function widget_disable(){
+<?php
+function widget_disable()
+{
   remove_theme_support('widgets-block-editor');
 }
-add_action('after_setup_theme','widget_disable');
+add_action('after_setup_theme', 'widget_disable');
 
-function custom_theme_menus() {
-  register_nav_menus( array(
-      'secondary-menu' => 'Menú Secundario',
-  ) );
+function custom_theme_menus()
+{
+  register_nav_menus(array(
+    'secondary-menu' => 'Menú Secundario',
+  ));
 }
-add_action( 'after_setup_theme', 'custom_theme_menus' );
+add_action('after_setup_theme', 'custom_theme_menus');
 
 
 /*widget nav*/
-function zona_widget(){
-    register_sidebar(
-    array('name'=>'imagen nav', 'id'=>'img_nav',// le damos nombre e id al footer
-          'before_widget'=>'<div id="%1$s" class="">', // añadimos clase e id 
-          'after_widget'=>'</div>',// cerramos el div
+function zona_widget()
+{
+  register_sidebar(
+    array(
+      'name' => 'imagen nav', 'id' => 'img_nav', // le damos nombre e id al footer
+      'before_widget' => '<div id="%1$s" class="disney-nav">', // añadimos clase e id 
+      'after_widget' => '</div>', // cerramos el div
     )
-    );
-  }
-  add_action('widgets_init','zona_widget');
+  );
+}
+add_action('widgets_init', 'zona_widget');
+
+/*widget carrusel*/
+function widget_carrusel()
+{
+  register_sidebar(
+    array(
+      'name' => 'carrusel 1', 'id' => 'carrusel_img1', // le damos nombre e id al footer
+      'before_widget' => '<div id="%1$s" class="carousel-item active">', // añadimos clase e id 
+      'after_widget' => '</div>', // cerramos el div
+    )
+  );
+  register_sidebar(
+    array(
+      'name' => 'carrusel 2', 'id' => 'carrusel_img2', // le damos nombre e id al footer
+      'before_widget' => '<div id="%1$s" class="carousel-item">', // añadimos clase e id 
+      'after_widget' => '</div>', // cerramos el div
+    )
+  );
+  register_sidebar(
+    array(
+      'name' => 'carrusel 3', 'id' => 'carrusel_img3', // le damos nombre e id al footer
+      'before_widget' => '<div id="%1$s" class="carousel-item">', // añadimos clase e id 
+      'after_widget' => '</div>', // cerramos el div
+    )
+  );
+ 
+}
+add_action('widgets_init', 'widget_carrusel');
--- wp-content/themes/woocommerce/home-page.php
+++ wp-content/themes/woocommerce/home-page.php
@@ -18,8 +18,6 @@ get_header();
 
 <main id="primary" class="site-main">
 
-	
-
 
 	<div id="carouselExampleIndicators" class="carousel slide">
 		<div class="carousel-indicators">
@@ -28,15 +26,13 @@ get_header();
 			<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
 		</div>
 		<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img src="wp-content/uploads/2023/06/carrusel1.png" class="d-block w-100" alt="...">
-			</div>
-			<div class="carousel-item">
-				<img src="wp-content/uploads/2023/06/carrusel2.png" class="d-block w-100" alt="...">
-			</div>
-			<div class="carousel-item">
-				<img src="wp-content/uploads/2023/06/carrusel3.png" class="d-block w-100" alt="...">
-			</div>
+
+			<?php if (is_active_sidebar('carrusel_img1')) : dynamic_sidebar('carrusel_img1');
+			endif; ?>
+			<?php if (is_active_sidebar('carrusel_img1')) : dynamic_sidebar('carrusel_img2');
+			endif; ?>
+			<?php if (is_active_sidebar('carrusel_img1')) : dynamic_sidebar('carrusel_img3');
+			endif; ?>
 		</div>
 		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -46,6 +42,19 @@ get_header();
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="visually-hidden">Next</span>
 		</button>
+	</div>
+
+	<div class="container">
+		<ul class="mt-5 rectangulo-pagos">
+			<li class="pagos">Paga comodo y seguro</li>
+			<li class="pagos"><i class="fa-brands fa-cc-visa me-3" style="color: #5697FA;"></i>Hasta 12 cuotas sin interés</li>
+			<li class="pagos"><i class="fa-regular fa-credit-card me-3" style="color: #5697FA;"></i>Targeta de debito</li>
+			<li class="pagos"><i class="fa-solid fa-plus me-3" style="color: #5697FA;"></i>Más medio de pago</li>
+		</ul>
+
+		<h2 class="mt-5">nuevos productos</h2>
+
+
 	</div>
 
 </main><!-- #main -->
